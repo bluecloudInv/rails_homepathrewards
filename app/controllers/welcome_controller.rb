61b599cf4e721f3a63c6f4ae3fa1d910bbbf28c5
santosh.natarajan <b@thevara.com>
2018-06-06 00:27:09 -0600

    add register and update images

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,4 +10,7 @@ class WelcomeController < ApplicationController
 
   def choose
   end
+
+  def register
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/eligible'
   get 'welcome/choose'
+  get 'welcome/register'
 
   root 'welcome#index'
 end
